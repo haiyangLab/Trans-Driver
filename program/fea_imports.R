1e07575f3e0ad3e85e8722dbd3a1179326d57dcc
haiyangLab <99403860+haiyangLab@users.noreply.github.com>
2022-03-26 20:28:04 +0800

    Update fea_imports.R

--- program/fea_imports.R
+++ program/fea_imports.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-databar1<-read.table('C:/Users/zhanglei/Desktop/论文材料/results_3/each_cancer_fea_3_4_11.txt', header=T, sep='\t', encoding="UTF-8")
+databar1<-read.table('../results/each_cancer_fea.txt', header=T, sep='\t', encoding="UTF-8")
 print(databar1)
 theme_bar <- function(..., bg='white'){
   require(grid)
@@ -9,24 +9,24 @@ theme_bar <- function(..., bg='white'){
           panel.background=element_rect(fill='transparent', color='black'),
           plot.title = element_text(hjust = 0.5, face = "bold",size = 14),
           panel.border=element_rect(fill='transparent', color='transparent'),
-          panel.grid=element_blank(),#去网格线
-          #axis.title.x = element_blank(),#去x轴标签
-          axis.title.y=element_text(face = "bold",size = 14),#y轴标签加粗及字体大小
-          axis.title.x=element_text(face = "bold",size = 14),#y轴标签加粗及字体大小
-          axis.title=element_text(face = "bold",size = 14),#y轴标签加粗及字体大小
-          axis.text = element_text(face = "bold",size = 12),#坐标轴刻度标签加粗
+          panel.grid=element_blank(),
+          #axis.title.x = element_blank(),
+          axis.title.y=element_text(face = "bold",size = 14),
+          axis.title.x=element_text(face = "bold",size = 14),
+          axis.title=element_text(face = "bold",size = 14),
+          axis.text = element_text(face = "bold",size = 12),
           axis.text.x = element_text(angle=45, hjust = 0.5, vjust = 0.5, face = "bold",size = 12),
-          # axis.ticks = element_line(color='black'),#坐标轴刻度线
+          # axis.ticks = element_line(color='black'),
           # axis.ticks.margin = unit(0.8,"lines"),
-          legend.title=element_blank(),#去除图例标题
-          # legend.justification=c(1,0),#图例在画布的位置(绘图区域外)
-          legend.position=c(0.87, 0.9),#图例在绘图区域的位置
-          # legend.position='top',#图例放在顶部
-          legend.direction = "vertical",#设置图例水平放置
+          legend.title=element_blank(),
+          # legend.justification=c(1,0),
+          legend.position=c(0.87, 0.9),
+          # legend.position='top',
+          legend.direction = "vertical",
           # legend.spacing.x = unit(2, 'cm'),
           legend.text = element_text(face = "bold",size = 12,margin = margin(r=20)),
           legend.background = element_rect( linetype="solid",colour ="black")
-          # legend.margin=margin(0,0,-7,0)#图例与绘图区域边缘的距离
+          # legend.margin=margin(0,0,-7,0)
           # legend.box.margin =margin(-10,0,0,0)
     )
   
@@ -36,6 +36,6 @@ q2<-ggplot(data=databar1, mapping=aes(x = Cancer, y = Importance,fill=Features))
     labs(title = "Features importance")+
     theme_bar()+ 
     coord_cartesian(ylim=c(0,0.75))+
-    scale_y_continuous(expand = c(0, 0))#消除x轴与绘图区的间隙
+    scale_y_continuous(expand = c(0, 0))
 q2
-#theme函数设置
+#theme鍑芥暟璁剧疆
